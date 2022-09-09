638eeb05940950ef4dd448b6f75cf277de7e27af
denis <denis@edu.yar.ru>
2022-09-09 13:14:05 +0300

    env api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+const theCode = process.env.REACT_APP_SUPER_SECRET_CODE;
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{margin: " 0 auto"}}>{theCode}</div>
     </div>
   );
 }
